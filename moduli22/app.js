b19b0c94be89e3e872b69b5be89dc9e6bebfd14a
mbresass <mbresasimnica17@gmail.com>
2024-08-20 18:05:23 +0200

    mdouli22

---
+++ moduli22/app.js
@@ -0,0 +1,95 @@
+console.log("Good job guys!")
+
+
+var x = 5
+
+//var 123 = 10 nuk duhet me i emertu variablat me numra
+//var *ds = 45 nuk duhet me ja nis me special characters
+
+
+var a = 50 + 50
+console.log(a)
+
+var b = 10
+var c = 20
+var d = b + c
+console.log(d)
+
+var g = c - b
+console.log(g)
+
+
+var k = 10
+var l = 4
+var m = k % 1
+console.log(m)
+
+var n = 99
+n++ //postfix
+console.log(n)
+
+var o = 101
+o--
+console.log(o)
+
+var p = 5
+var q = 2
+var h = p**q
+console.log(h)
+
+var e = 15
+e +=5 // e= 15+5
+console.log(e)
+
+e -=5
+console.log(e) // 15
+
+var u = 10
+u *=3 // 10 * 3
+console.log(u) // u = 30
+
+u /=2
+console.log(u)
+
+var t = 5
+var i = 5
+console.log(t == i)
+
+var n1 = 6
+var n2 = 7
+console.log(n1 !=n2) // true
+
+console.log(n1 > n2) // 6>7 false
+console.log(n1 < n2) // 6<7 true
+
+var numri = 123
+var tekst = "123"
+console.log(numri == tekst) // false
+
+var firsName = "Digital"
+var lastName = "School"
+var fullName = firstName + " " lastName
+console.log(fullName)
+
+var school = "Shkolla Digjitale"
+console.log(`Hello from ${school}`)
+
+
+// AND operator- duhet me i plotesu krejt kushtet qe me dal shprehja TRUE
+var age = 24 
+console.log(age >=18 && age <=65)
+
+// OR operator= vetem nese nje kusht eshte TRUE, shprehja na kthen TRUE
+var num1 = 5
+var num2 = 6
+console.log(num1==5 || num2 == 5)
+
+
+var num3 = 9
+var num4 = 10
+console.log(!(num3==num4))
+
+
+
+
+
