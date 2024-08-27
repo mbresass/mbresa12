98368fcf7297fd400bcded3bb4d849c2362b2afe
mbresass <mbresasimnica17@gmail.com>
2024-08-27 17:19:39 +0200

    moduli27

--- moduli27/js/main.js
+++ moduli27/js/main.js
@@ -2,7 +2,7 @@ for(var i = 0; i<5; i++){
     console.log("Vlera e i=së është:" + i);
 }
 
-console.log("------------------");
+console.log("-----------------------");
 
 var personat = {emri: "Mbresa", mbiemri: "Simnica", mosha: 17};
 
@@ -16,20 +16,41 @@ for(z in personat){
 }
 console.log(text);
 
-console.log("------------------");
+console.log("-----------------------");
 
 var emrat = ['Altina', 'Andi', 'Anila'];
 
 var y;
 
 for(y of emrat){
-    console.log(y)
+    console.log(y);
 }
 
-console.log("------------------")
+console.log("-----------------------");
 var f = 0;
 
 do{
     f=f+1;
     console.log(f);
 }while(f<5);
+
+console.log("-----------------------");
+
+var x = 0
+while(x<17){
+    x=x+1
+    console.log(x);
+}
+
+console.log("-----------------------");
+
+var l = 0;
+var c = 0;
+
+while(l<3){
+    l++;
+    c+= l;
+    console.log(l);
+    console.log(c);
+}
+
