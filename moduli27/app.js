ed7ff88b06e05e09b212139164fc91da44febfb4
mbresass <mbresasimnica17@gmail.com>
2024-08-29 16:20:36 +0200

    moduli27

---
+++ moduli27/app.js
@@ -0,0 +1,29 @@
+//do while loop
+
+var i = 0;
+
+do{
+    //i ++
+    i = i + 1
+    console.log(i)
+
+}while(i < 5)
+
+console.log("----------------------")
+
+//While Loop
+
+var j = 0 
+
+while(j < 5){
+    j++
+    console.log(j)
+}
+
+console.log("----------------------")
+
+// for loop
+for(var i = 0; i < 6; i++){
+    console.log("Value of i is:" + i)
+}
+
